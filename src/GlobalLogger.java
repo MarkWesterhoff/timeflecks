6257b0a1608b0a7d44850803cf97cc352eb6143f
Andrew Faulds <faulda@rpi.edu>
2014-10-15 13:09:02 -0400

    Add GlobalLogger

---
+++ src/GlobalLogger.java
@@ -0,0 +1,20 @@
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+/**
+ * Singleton class to hold to global logger for the TimeFlecks application.
+ * @author Andrew
+ */
+public class GlobalLogger {
+	private static Logger logger;
+	
+	private GlobalLogger() { }
+	
+	public static Logger getLogger() {
+		if (logger == null) {
+			logger = Logger.getLogger(GlobalLogger.class.getName());
+			logger.setLevel(Level.INFO);
+		}
+		return logger;
+	}
+}
