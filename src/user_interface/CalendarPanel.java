5f551c99593be9745b4cfdf5eb03a7e35baa0046
Eric Kinnear <eric.kinnear@gmail.com>
2014-10-26 03:40:54 -0400

    Added calendar panel classes

---
+++ src/user_interface/CalendarPanel.java
@@ -0,0 +1,17 @@
+package user_interface;
+
+import javax.swing.*;
+
+public class CalendarPanel extends JComponent
+{
+
+	/**
+	 * Auto generated default serial version UID
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	
+	
+}
---
+++ src/user_interface/test/CalendarPanelTest.java
@@ -0,0 +1,27 @@
+package user_interface.test;
+
+import javax.swing.*;
+
+import org.junit.Test;
+
+public class CalendarPanelTest
+{
+
+	/**
+	 * Auto generated default serial version UID
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Test
+	public void testCalendarPanel()
+	{
+		JFrame f = new JFrame("Calendar Panel Test");
+		
+		
+	}
+	
+	
+	
+	
+	
+}
