3deb8982fc5ef9f36e16ddf9f04675fc15e5de1b
Eric Kinnear <eric.kinnear@gmail.com>
2014-10-26 03:41:01 -0400

    Added task component classes

---
+++ src/user_interface/test/TaskComponentTest.java
@@ -0,0 +1,27 @@
+package user_interface.test;
+
+import javax.swing.*;
+
+import org.junit.Test;
+
+public class TaskComponentTest
+{
+
+	/**
+	 * Auto generated default serial version UID
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Test
+	public void testTaskComponent()
+	{
+		JFrame f = new JFrame("Task Component Test");
+		
+		
+	}
+	
+	
+	
+	
+	
+}
