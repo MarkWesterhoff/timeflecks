eed6365b1c203aeedb66eabd2b2f83173ad5d795
Eric Kinnear <eric.kinnear@gmail.com>
2014-10-26 05:02:30 -0400

    Updated tests for calendar and task components

--- src/user_interface/test/CalendarPanelTest.java
+++ src/user_interface/test/CalendarPanelTest.java
@@ -1,8 +1,12 @@
 package user_interface.test;
 
+import java.awt.FlowLayout;
+
 import javax.swing.*;
 
 import org.junit.Test;
+
+import user_interface.CalendarPanel;
 
 public class CalendarPanelTest
 {
@@ -11,17 +15,65 @@ public class CalendarPanelTest
 	 * Auto generated default serial version UID
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	@Test
 	public void testCalendarPanel()
 	{
-		JFrame f = new JFrame("Calendar Panel Test");
-		
-		
+		try
+		{
+			JFrame newFrame = new JFrame("Calendar Test");
+			JPanel container = new JPanel();
+
+			FlowLayout panelLayout = new FlowLayout();
+			panelLayout.setHgap(0);
+			panelLayout.setVgap(0);
+			container.setLayout(panelLayout);
+
+			JScrollPane s = new JScrollPane(container);
+
+			int width = 150;
+			int height = 1000;
+
+			for (int i = 0; i < 7; i++)
+			{
+				CalendarPanel p;
+				if (i == 0)
+				{
+					p = new CalendarPanel(true, true, width, height);
+				}
+				else if (i == 6)
+				{
+					p = new CalendarPanel(false, false, width, height);
+				}
+				else
+				{
+					p = new CalendarPanel(false, true, width, height);
+				}
+
+				container.add(p);
+
+			}
+
+			// container.setSize(400,400);
+
+			newFrame.getContentPane().add(s);
+
+			newFrame.pack();
+
+			newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+			newFrame.setSize(400, 400);
+			newFrame.setAutoRequestFocus(true);
+			newFrame.setResizable(true);
+
+			newFrame.setVisible(true);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Unable to " + e);
+		}
+
 	}
-	
-	
-	
-	
-	
+
 }
--- src/user_interface/test/TaskComponentTest.java
+++ src/user_interface/test/TaskComponentTest.java
@@ -1,8 +1,14 @@
 package user_interface.test;
 
+import java.awt.Dimension;
+import java.awt.Rectangle;
+
 import javax.swing.*;
 
 import org.junit.Test;
+
+import user_interface.TaskComponent;
+import core.Task;
 
 public class TaskComponentTest
 {
@@ -11,17 +17,37 @@ public class TaskComponentTest
 	 * Auto generated default serial version UID
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	@Test
 	public void testTaskComponent()
 	{
-		JFrame f = new JFrame("Task Component Test");
-		
-		
+		try
+		{
+			Task t = new Task("New Task Blah Blah Blah");
+			TaskComponent p = new TaskComponent(t, new Rectangle(10, 10, 200,
+					100));
+			JPanel panel = new JPanel();
+			panel.add(p);
+			panel.setPreferredSize(new Dimension(400, 400));
+			JFrame newFrame = new JFrame("Task Component Test");
+
+			newFrame.getContentPane().add(p);
+
+			newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+			newFrame.setSize(400, 400);
+
+			newFrame.setAutoRequestFocus(true);
+			newFrame.setResizable(true);
+
+			newFrame.setVisible(true);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "Unable to " + e);
+		}
+
 	}
-	
-	
-	
-	
-	
+
 }
