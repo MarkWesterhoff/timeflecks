e16179c27d49bf93910ae5859e32463f121a575f
Eric Kinnear <eric.kinnear@gmail.com>
2014-10-27 01:21:21 -0400

    Initial implementation of drawing components on calendar
    Currently hardcoded coordinates, more coming soon

--- src/user_interface/CalendarPanel.java
+++ src/user_interface/CalendarPanel.java
@@ -2,10 +2,15 @@ package user_interface;
 
 import java.awt.*;
 import java.awt.font.FontRenderContext;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.logging.Level;
 
 import javax.swing.*;
 
+import core.Task;
+import core.Timeflecks;
 import logging.GlobalLogger;
 
 public class CalendarPanel extends JPanel
@@ -13,18 +18,25 @@ public class CalendarPanel extends JPanel
 	private boolean drawTimes;
 	private boolean drawRightSideLine;
 
+	private Date date;
+	private ArrayList<Task> tasksToPaint;
+
 	/**
 	 * Auto generated default serial version UID
 	 */
 	private static final long serialVersionUID = 1L;
 
-	public CalendarPanel(boolean drawTimes, boolean drawRightSideLine,
-			int width, int height)
+	public CalendarPanel(Date date, boolean drawTimes,
+			boolean drawRightSideLine, int width, int height)
 	{
 		super();
 
 		this.drawTimes = drawTimes;
 		this.drawRightSideLine = drawRightSideLine;
+
+		this.setDate(date);
+		setTasksToPaint(new ArrayList<Task>());
+		refresh();
 
 		setBorder(BorderFactory.createEmptyBorder());
 		// setBorder(BorderFactory.createLineBorder(Color.red));
@@ -103,19 +115,20 @@ public class CalendarPanel extends JPanel
 			leftInset = g.getFontMetrics(g.getFont()).stringWidth("12pm")
 					+ insetFromLeft;
 		}
-		
+
 		// Get the height of the font to draw the lines
 		Graphics2D g2 = (Graphics2D) g;
 		FontRenderContext frc = g2.getFontRenderContext();
 		int fontHeight = (int) g2.getFont().getLineMetrics("12pm", frc)
-						.getHeight();
+				.getHeight();
 
 		if (drawRightSideLine)
 		{
 			rightInset = 8 + insetFromRight;
 		}
 
-		for (int insetFromTop = d.height / 24 - (fontHeight / 2); insetFromTop < d.height - (fontHeight / 2); insetFromTop += d.height / 24)
+		for (int insetFromTop = d.height / 24 - (fontHeight / 2); insetFromTop < d.height
+				- (fontHeight / 2); insetFromTop += d.height / 24)
 		{
 			g.drawLine(leftInset, insetFromTop, d.width - rightInset,
 					insetFromTop);
@@ -131,15 +144,72 @@ public class CalendarPanel extends JPanel
 		}
 
 		// Go through and draw any tasks at the appropriate place
+		for (Task t : tasksToPaint)
+		{
+//			int firstInset = d.height / 24 - (fontHeight / 2);
+//			
+//			int hourIncrement = d.height / 24;
+//			
+//			Calendar calendar = Calendar.getInstance();
+//			calendar.setTime(t.getStartTime());
+//			int taskHours = calendar.get(Calendar.HOUR_OF_DAY);
+//					
+//			int durationInHours = ((int)t.getDuration() / 1000 / 60 / 60) % 24;
+//			
+//			TaskComponent tc = new TaskComponent(t, new Rectangle(leftInset, firstInset + taskHours * hourIncrement, d.width - rightInset, durationInHours * hourIncrement));
+//			add(tc);
+			
+			TaskComponent tc = new TaskComponent(t, new Rectangle(1, 1, 100, 200));
+			add(tc);
+		}
 	}
-	
+
 	/**
-	 * refreshes the calendar view, re-adds the tasks for that day from the TaskList. 
-	 * Warning: This may be time-intensive for large TaskLists
+	 * refreshes the calendar view, re-adds the tasks for that day from the
+	 * TaskList. Warning: This may be time-intensive for large TaskLists
 	 */
 	public void refresh()
 	{
-		GlobalLogger.getLogger().logp(Level.INFO, "CalendarPanel", "refresh", "Refresh called.");
+		GlobalLogger.getLogger().logp(Level.INFO, "CalendarPanel", "refresh",
+				"Refresh called.");
+
+		// We need to go through the task list and update all tasks that match
+		// the current date
+		
+		// Clear it out first
+		tasksToPaint.clear();
+		
+		for (Task t : Timeflecks.getSharedApplication().getTaskList()
+				.getTasks())
+		{
+			// Same day will just return false if they are null
+			if (sameDay(t.getStartTime(), this.getDate()))
+			{
+				// They are on the same day
+				tasksToPaint.add(t);
+			}
+		}
+		
+		// Now we have the list of tasks to paint, we want to make sure that they are painted in paintComponent. 
+	}
+
+	public boolean sameDay(Date firstDate, Date secondDate)
+	{
+		if (firstDate == null || secondDate == null)
+		{
+			return false;
+		}
+		
+		Calendar c1 = Calendar.getInstance();
+		Calendar c2 = Calendar.getInstance();
+
+		c1.setTime(firstDate);
+		c2.setTime(secondDate);
+
+		boolean same = c1.get(Calendar.YEAR) == c2.get(Calendar.YEAR)
+				&& c1.get(Calendar.DAY_OF_YEAR) == c2.get(Calendar.DAY_OF_YEAR);
+
+		return same;
 	}
 
 	public static void main(String[] args)
@@ -164,15 +234,15 @@ public class CalendarPanel extends JPanel
 				CalendarPanel p;
 				if (i == 0)
 				{
-					p = new CalendarPanel(true, true, width, height);
+					p = new CalendarPanel(new Date(), true, true, width, height);
 				}
 				else if (i == 6)
 				{
-					p = new CalendarPanel(false, false, width, height);
+					p = new CalendarPanel(new Date(), false, false, width, height);
 				}
 				else
 				{
-					p = new CalendarPanel(false, true, width, height);
+					p = new CalendarPanel(new Date(), false, true, width, height);
 				}
 
 				container.add(p);
@@ -198,6 +268,26 @@ public class CalendarPanel extends JPanel
 			e.printStackTrace();
 			JOptionPane.showMessageDialog(null, "Unable to " + e);
 		}
+	}
+
+	public ArrayList<Task> getTasksToPaint()
+	{
+		return tasksToPaint;
+	}
+
+	public void setTasksToPaint(ArrayList<Task> tasksToPaint)
+	{
+		this.tasksToPaint = tasksToPaint;
+	}
+
+	public Date getDate()
+	{
+		return date;
+	}
+
+	public void setDate(Date date)
+	{
+		this.date = date;
 	}
 
 }
