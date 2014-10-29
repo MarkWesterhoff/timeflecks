06088df588f110e57257eede1754e7facae796cc
breadknock <brenoc@rpi.edu>
2014-10-29 02:40:09 -0400

    Ignore case when comparing task names (changed core.Task.nameComparator)

--- src/core/Task.java
+++ src/core/Task.java
@@ -218,7 +218,7 @@ public class Task implements Scheduleable, Serializable
 	{
 		public int compare(Task t1, Task t2)
 		{
-			return t1.name.compareTo(t2.name);
+			return t1.name.compareToIgnoreCase(t2.name);
 		}
 	};
 
